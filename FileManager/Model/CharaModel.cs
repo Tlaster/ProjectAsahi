5233d1f84e9b656a5f0ce25faed0b82691eff6b6
Tlaster <tlaster@outlook.com>
2015-10-17 20:02:10 +0800

    bug fix when Load&Save

--- FileManager/Model/CharaModel.cs
+++ FileManager/Model/CharaModel.cs
@@ -1,11 +1,19 @@
-﻿namespace FileManager.Model
+﻿using System.Runtime.Serialization;
+
+namespace FileManager.Model
 {
+    [DataContract]
     public sealed class CharaModel
     {
+        [DataMember]
         public float Position_X { get; set; }
+        [DataMember]
         public float Position_Y { get; set; }
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public string FilePath { get; set; }
+        [DataMember]
         public int Deep { get; set; }
     }
 }
--- FileManager/Model/SaveModel.cs
+++ FileManager/Model/SaveModel.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace FileManager.Model
 {
+    [DataContract]
     public sealed class SaveModel
     {
+        [DataMember]
         public int SaveIndex { get; set; }
+        [DataMember]
         public string CurrentFilePath { get; set; }
+        [DataMember]
         public string BackgroundPath { get; set; }
+        [DataMember]
         public string BGMPath { get; set; }
+        [DataMember]
         public int BlockPosition { get; set; }
+        [DataMember]
         public BoolSetting IsMultipleLanguage { get; set; }
+        [DataMember]
         public StringSetting TextLayoutBackground { get; set; }
+        [DataMember]
         public FloatSetting ImageHeight { get; set; }
+        [DataMember]
         public FloatSetting ImageWidth { get; set; }
+        [DataMember]
         public FloatSetting FontSize { get; set; }
+        [DataMember]
         public string NextFilePath { get; set; }
+        [DataMember]
         public IList<CharaModel> CharaList { get; set; }
+        [DataMember]
         public string SaveTime { get; set; }
     }
+    [DataContract]
     public sealed class StringSetting
     {
         public StringSetting(string setvalue,bool isglobal,bool issetted)
@@ -26,10 +42,14 @@ namespace FileManager.Model
             IsGlobal = isglobal;
             IsSetted = issetted;
         }
+        [DataMember]
         public string Value { get; set; }
+        [DataMember]
         public bool IsGlobal { get; set; }
+        [DataMember]
         public bool IsSetted { get; set; }
     }
+    [DataContract]
     public sealed class BoolSetting
     {
         public BoolSetting(bool setvalue, bool isglobal, bool issetted)
@@ -38,10 +58,14 @@ namespace FileManager.Model
             IsGlobal = isglobal;
             IsSetted = issetted;
         }
+        [DataMember]
         public bool Value { get; set; }
+        [DataMember]
         public bool IsGlobal { get; set; }
+        [DataMember]
         public bool IsSetted { get; set; }
     }
+    [DataContract]
     public sealed class FloatSetting
     {
         public FloatSetting(float setvalue, bool isglobal, bool issetted)
@@ -50,8 +74,11 @@ namespace FileManager.Model
             IsGlobal = isglobal;
             IsSetted = issetted;
         }
+        [DataMember]
         public float Value { get; set; }
+        [DataMember]
         public bool IsGlobal { get; set; }
+        [DataMember]
         public bool IsSetted { get; set; }
     }
 }
